2a238fabe65414da948742973ca96ddf4e162653
MicKr4ne <michael.kranl@gmail.com>
2023-05-05 15:27:26 +0200

    add subrepos

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -6,8 +6,9 @@ from PIL import Image
 import numpy as np
 from matplotlib import pyplot as plt
 import psnr_hvsm as psnr
+import seaborn as sns
 
-result_path = r".\metrics_example.csv"
+result_path = r"D:\DEV\Thesis\Denoising\mixed_syn_rain2_.csv"
 #result_path_psnr = r"D:\DEV\Thesis\Denoising\mixed_syn_rain2_psnr.csv"
 
 # df = pd.DataFrame(columns=['Image', 'Restormer_UQI', 'Restormer_SSIM', 'Rescan_UQI', 'Rescan_SSIM'])
@@ -45,35 +46,35 @@ def calc_benchmark():
             restormer = np.asarray(Image.open(join(restormer_path, img + ext)))
             rescan = np.asarray(Image.open(join(rescan_path, img + ext)))
 
-            #restormer_uqi = sw.uqi(ref, restormer)
             restormer_ssim = sw.ssim(ref, restormer)
             restormer_psnr = psnr_hvsm_rgb(ref, restormer)
 
-            #rescan_uqi = sw.uqi(ref, rescan)
             rescan_ssim = sw.ssim(ref, rescan)
             rescan_psnr = psnr_hvsm_rgb(ref, rescan)
             
             scores.append([restormer_ssim[0], rescan_ssim[0], restormer_psnr, rescan_psnr])
-            #scores_psnr.append([restormer_psnr, rescan_psnr])
             pics.append(img+ext)
 
         df = pd.DataFrame(scores, columns=['Restormer_SSIM', 'Rescan_SSIM', 'Restormer_PSNR', 'Rescan_PSNR'], index=pics)
-        #df_psnr = pd.DataFrame(scores_psnr, columns=['Restormer_PSNR', 'Rescan_PSNR'], index=pics)
         df.to_csv(result_path, index=True)
-        #df_psnr.to_csv(result_path_psnr, index=True)
 
+    plt.rcParams.update({'font.size': 16})
     fig, ax = plt.subplots(1, 2, figsize=(10, 5))
-    bp1 = df.boxplot(column=['Restormer_SSIM', 'Rescan_SSIM'], ax=ax[0])
-    bp2 = df.boxplot(column=['Restormer_PSNR', 'Rescan_PSNR'], ax=ax[1])
+    # bp1 = df.boxplot(column=['Restormer_SSIM', 'Rescan_SSIM'], ax=ax[0])
+    # bp2 = df.boxplot(column=['Restormer_PSNR', 'Rescan_PSNR'], ax=ax[1])
     
-    fig.suptitle('Benchmark results for mixed_syn_rain2 dataset')
-    bp1.set_title('SSIM scores')
-    bp1.set_ylabel('Score')
-    bp2.set_title('PSNR-HVS-M scores')
-    bp2.set_ylabel('Score')
-    # boxplot.set_ylabel('Score')
-    # boxplot.set_xlabel('Denoiser')
-    # boxplot.set_xticklabels(['Restormer UQI', 'Restormer SSIM', 'Restormer PSNR', 'Rescan UQI', 'Rescan SSIM', 'Rescan PSNR'])
+    violin1 = sns.violinplot(data=df[['Restormer_SSIM', 'Rescan_SSIM']], ax=ax[0], palette="rainbow")
+    voilin2 = sns.violinplot(data=df[['Restormer_PSNR', 'Rescan_PSNR']], ax=ax[1], palette="turbo")
+    fig.suptitle('Benchmark results for syn_derain dataset')
+    violin1.set_title('SSIM scores')
+    violin1.set_ylabel('Score')
+    voilin2.set_title('PSNR-HVS-M scores')
+    voilin2.set_ylabel('Score')
+    #bp1.set_title('SSIM scores')
+    #bp1.set_ylabel('Score')
+    #bp2.set_title('PSNR-HVS-M scores')
+    #bp2.set_ylabel('Score')
+
     plt.show()
 
 def main():
